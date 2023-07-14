f10f9d6797847d7077dd90da41eff92473914e23
Nikita <bananchik26@gmail.com>
2023-07-14 11:21:44 +0300

    fix: out

--- test.sh
+++ test.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-echo $(git rev-list master..pr-test)
+> out.txt
+message=$(git rev-list origin/master..origin/pr-test)
+echo $message
+message=$(echo $message | tr '\n' ' ')
+echo $message
+echo "text=$message" >> out.txt
+
+# echo "[" >> out.txt
+# for commit in $(git rev-list origin/master..origin/pr-test); do
+#     echo $commit, >> out.txt
+# done
+# echo "]" >> out.txt
