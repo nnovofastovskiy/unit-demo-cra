c3535c7238dd91818c2aa2c2b8672aa395ae0a62
Nikita <bananchik26@gmail.com>
2023-07-15 01:35:21 +0300

    test matrix

--- test.sh
+++ test.sh
@@ -1,33 +1,14 @@
 #!/bin/bash
 
-> out.txt
-message=$(git rev-list origin/master..origin/pr-test)
-echo $message
-echo "${#message[@]}"
-message=$(echo $message | tr '\n' ' ')
-# echo $message
-echo "text=$message" >> out.txt
-
-IFS=' ' read -r -a array <<< "$message"
-echo array
-echo "${#array[@]}"
-# echo "${array}"
-
-# echo "[" >> out.txt
-# for commit in $(git rev-list origin/master..origin/pr-test); do
-#     echo $commit, >> out.txt
-# done
-# echo "]" >> out.txt
+commmits=$(git rev-list origin/master..origin/pr-test)
+commmits=$(echo $commmits | tr '\n' ' ')
+IFS=' ' read -r -a array <<< "$commmits"
 
 commits=$(git rev-list origin/master..origin/pr-test)
 IFS=' ' read -r -a commitsArray <<< "$commits"
-echo ${commitsArray[@]}
-# for commit in ${commitsArray[@]}; do
-#     echo {\"sha\":\"$commit\"}
-# done
 commitsObject="{\"include\":[
-    for commit in ${commitsArray[@]}; do
+    $(for commit in ${array[@]}; do
         echo {\"sha\":\"$commit\"}
-    done
+    done)
 ]}"
 echo "matrix=$commitsObject"
