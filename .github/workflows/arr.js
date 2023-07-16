5be4df739b2c9df11c89a65d595efcde5655c05b
Nikita <bananchik26@gmail.com>
2023-07-16 21:03:06 +0300

    fix arr js

--- .github/workflows/arr.js
+++ .github/workflows/arr.js
@@ -1,2 +1,2 @@
-const commits = process.env.NODE_ENV.trim().split('\n').map(item => ({ sha: item }));
+const commits = process.env.NODE_ENV.trim().split('\n').map(item => ({ "sha": item }));
 console.log(commits)
