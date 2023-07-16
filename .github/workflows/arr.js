225dba772377f3cef3d3435727b47efe3d9c2ee9
Nikita <bananchik26@gmail.com>
2023-07-16 20:56:30 +0300

    arr js

---
+++ .github/workflows/arr.js
@@ -0,0 +1,4 @@
+// console.log('test.js');
+
+const commits = process.env.NODE_ENV.trim().replace(/\n/g, ' ').split(' ');
+console.log(commits)
--- test.sh
+++ test.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-commmits=$(git rev-list origin/master..origin/pr-test)
-commmits=$(echo $commmits | tr '\n' ' ')
-IFS=' ' read -r -a array <<< "$commmits"
+export NODE_ENV=$(git rev-list origin/master..origin/pr-test)
+commits=$(node test.js)
+# echo $commits
+# export NODE_ENV=${{commits}}
+echo $commits
+# commmits=$(echo $commmits | tr '\n' ' ')
+# IFS=' ' read -r -a array <<< "$commmits"
 
-commits=$(git rev-list origin/master..origin/pr-test)
-IFS=' ' read -r -a commitsArray <<< "$commits"
-commitsObject="{\"include\":[
-    $(for commit in ${array[@]}; do
-        echo {\"sha\":\"$commit\"}
-    done)
-]}"
-echo "matrix=$commitsObject"
+# commits=$(git rev-list origin/master..origin/pr-test)
+# IFS=' ' read -r -a commitsArray <<< "$commits"
+# commitsObject="{\"include\":[
+#     $(for commit in ${array[@]}; do
+#         echo {\"sha\":\"$commit\"}
+#     done)
+# ]}"
+# echo "matrix=$commitsObject"
